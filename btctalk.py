28e0d5599d08cbeefb522739dec3e65d1a8c2c44
cdenaro <carlo.denaro@lastminute.com>
2018-07-24 18:18:12 +0200

    convert to p2

--- btctalk.py
+++ btctalk.py
@@ -2,7 +2,7 @@ from lxml import html
 import requests
 import sys
 
-print """
+print ("""
   bitcointalk.org intelligence scraping data
    _     _       _        _ _    _       _   
   | |__ | |_ ___| |_ __ _| | | _(_)_ __ | |_ 
@@ -12,7 +12,7 @@ print """
 
   maded with love <3 by blackout314 @ ghlab
   -------------------------------------------
-"""
+""")
 
 
 if len(sys.argv) > 1:
@@ -22,35 +22,35 @@ if len(sys.argv) > 1:
   profilepage = requests.get(profile)
   ptree = html.fromstring(profilepage.content)
   pdata = ptree.xpath("//td[@class='windowbg']/table/tr/td/text()")
-  print '[USER]', pdata[0]
-  print ' - (posts)', pdata[1]
-  print ' - (activity)', pdata[2]
-  print ' - (rank)', pdata[3]
-  print ' - (registered)', pdata[4]
-  print ' - (address)', pdata[9]
+  print ('[USER]', pdata[0])
+  print (' - (posts)', pdata[1])
+  print (' - (activity)', pdata[2])
+  print (' - (rank)', pdata[3])
+  print (' - (registered)', pdata[4])
+  print (' - (address)', pdata[9])
 
   url = "https://bitcointalk.org/index.php?action=profile;u=" + uid + ";sa=statPanel"
   page = requests.get(url)
   tree = html.fromstring(page.content)
 
   online = tree.xpath("//td[@class='windowbg2'][1]/table[1]/tr/td[2]/text()")
-  print '[DATA]'
+  print ('[DATA]')
   for d in online:
-    print ' >', d
+    print (' >', d)
 
   boards = tree.xpath("//td[@class='windowbg2'][2]/table/tr/td/a/text()")
   boards_perc = tree.xpath("//td[@class='windowbg2'][2]/table/tr/td/text()")
-  print '[BOARDS]'
+  print ('[BOARDS]')
   for i in range(len(boards)):
-    print ' +', boards_perc[i], "", boards[i]
+    print (' +', boards_perc[i], "", boards[i])
 
   pboards = tree.xpath("//td[@class='windowbg2'][1]/table/tr/td/a/text()")
   pboards_perc = tree.xpath("//div[@id='bodyarea']/table/tr/td/table/tr[6]/td[@class='windowbg2'][1]/table/tr/td/text()")
-  print '[BOARDS]'
+  print ('[BOARDS]')
   for i in range(len(pboards)):
-    print ' +', pboards_perc[i], "", pboards[i]
+    print (' +', pboards_perc[i], "", pboards[i])
 
 else:
-  print """
+  print ("""
   usage: python btctalk.py {UID}
-  """
+  """)
